db3ef0f33593d9ee5baf66d4d24f1f17d4879cd0
Sarad Mishra <saradmishra28@gmail.com>
2022-09-08 11:05:11 +0530

    creating KNN file from scratch

---
+++ KNN.py
@@ -0,0 +1,21 @@
+class KNN:
+    def __init__(self,k):
+        #defining # nearest neighbors
+        self.k=k
+    
+    def fit(self,X_train,y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def _distance(self, x):
+        return np.sqrt(sum(X_train-x)**2)
+
+    def _predict(self,x_test):
+        pass
+
+
+    def predict(self,X_test):
+        predict_list = []
+        pass
+if __name__=="__main__":
+    print('hello')        
